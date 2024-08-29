7c5fe6d8a1e4affbce97b7fe22f90cab4aa63263
gebeen-dev <rrdd1207@gmail.com>
2024-08-29 11:28:04 +0900

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int diff = sol.solution(100,20);
+        System.out.println(diff);
+    }
+}
