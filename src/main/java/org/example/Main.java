4ed1218a65e260e711032262f9f5bceee481fd74
gebeen-dev <rrdd1207@gmail.com>
2024-08-30 11:41:39 +0900

    Main 클래스 제거 및 NumOfMin 클래스 생성

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int diff = sol.solution(100,20);
-        System.out.println(diff);
-    }
-}
--- src/main/java/org/example/NumOfMin.java
+++ src/main/java/org/example/NumOfMin.java
@@ -1,6 +1,12 @@
 package org.example;
 
-class Solution {
+class NumOfMin {
+    public static void main(String[] args) {
+        NumOfMin sol = new NumOfMin();
+        int diff = sol.solution(100,20);
+        System.out.println(diff);
+    }
+
     public int solution(int num1, int num2) {
         if (-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000) {
             int answer = num1 - num2;
